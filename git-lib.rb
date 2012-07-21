c13502b26037156f2136e13cfa3d90cdfedd0d08
Tomas Lundell <tomas@two-bulls.com>
2012-07-22 00:06:10 +1000

    Simplify git-lib usage
    * Update git-lib to support latest git-host
    * Remove create command - push creates if not present
    * Remove add command - pull adds if not present

--- git-lib.rb
+++ git-lib.rb
@@ -25,7 +25,7 @@ end
 # Options
 
 options = {}
-options[:account] = getconfig("host.account")
+options[:account] = getconfig("host.default")
 options[:refspec] = 'master'
 options[:description] = ''
 
@@ -37,28 +37,36 @@ option_parser = OptionParser.new do |opts|
   opts.banner = 
 "
 Usage: git-lib [options] command lib-name
-command: create|add|push|pull
+
+Command description:
+  push      Pushes a new library. Creates if it doesn't exist.
+  pull      Pulls from a library. Creates if it doesn't exist.
+
 "
 
-    opts.on("--account [ACCOUNT]", "The repository account used for git-host. If omitted, read from git config.host.account.") do |val|
+    opts.on("--account [ACCOUNT]", "The repository account used for git-host. If omitted, the default account is used.") do |val|
         options[:account] = val
     end
 
     opts.on("--prefix [PREFIX]", "The path to the lib. If omitted, pwd/<lib-name> is used.") do |val|
-        options[:prefix] = val
+        options[:prefix] = val2bpivotaltracker
         prefix_specified = true
     end
 
-    opts.on("--description [DESCRIPTION]", "(create only): Adds a description to the repository.") do |val|
+    opts.on("--description [DESCRIPTION]", "(First push only): Adds a description to the repository.") do |val|
         options[:description] = val
     end
 
-    opts.on("--branch [REFSPEC]", "(push, pull only): Specifies the remote branch to sync with. Default is master.") do |val|
+    opts.on("--branch [REFSPEC]", "Specifies the remote branch to sync with. Default is master.") do |val|
         options[:refspec] = val
     end
 
     opts.on("--url [URL]", "The remote url. Inferred from host if not specified.") do |val|
         options[:url] = val
+    end
+
+    opts.on("--password [PASSWORD]", "The password. Not needed if password is stored in account.") do |val|
+        options[:password] = val
     end
 
     opts.on_tail("-h", "--help", "Show this message.") do
@@ -79,6 +87,7 @@ githost = 'git host'
 def host_options(options)
     account = options[:account] && "--account #{options[:account]} " || ""
     desc = options[:description] && "--description \"#{options[:description]}\" " || ""
+    password = options[:password] && "--password \"#{options[:password]}\" " || ""
     return account + desc
 end
 
@@ -88,15 +97,6 @@ def probe_repository(url)
 end
 
 commands = {
-    "create" => lambda do
-        system("#{githost} create-repo #{options[:libname]} #{host_options(options)}")
-        commands["push"].call() if $?.success?
-    end,
-
-    "add" => lambda do
-        message = "Add lib \"#{options[:libname]}\""
-        run "#{gitsubtree} add --message '#{message}' --prefix #{options[:prefix]} #{options[:url]} #{options[:refspec]}"
-    end,
 
     "push" => lambda do
         abort "No such directory '#{options[:libname]}'" unless Dir.exists? options[:prefix]
@@ -104,17 +104,24 @@ commands = {
         # If the repository doesn't exist, it's created for you
         if !probe_repository(options[:url])
             puts "Repository doesn't exist, creating..."
-            commands["create"].call()
-        else
-            superproject_name = File.split(gitdir)[1]
-            annotation = "(*#{superproject_name})"
-            message = "Pushed lib \"#{options[:libname]}\""
-            sha, success = call "#{gitsubtree} split --message '#{message}' --annotate '#{annotation}' --rejoin --prefix #{options[:prefix]}"
-            run "git push #{options[:url]} #{sha}:refs/heads/#{options[:refspec]}" unless not success
+            system("#{githost} create-repo #{options[:libname]} #{host_options(options)}")
         end
+
+        superproject_name = File.split(gitdir)[1]
+        annotation = "(*#{superproject_name})"
+        message = "Pushed lib \"#{options[:libname]}\""
+        sha, success = call "#{gitsubtree} split --message '#{message}' --annotate '#{annotation}' --rejoin --prefix #{options[:prefix]}"
+        run "git push #{options[:url]} #{sha}:refs/heads/#{options[:refspec]}" unless not success
     end,
 
     "pull" => lambda do
+        if !Dir.exists? options[:prefix] then
+            abort "No such repository: '#{options[:url]}'" unless probe_repository(options[:url])
+
+            message = "Add lib \"#{options[:libname]}\""
+            run "#{gitsubtree} add --message '#{message}' --prefix #{options[:prefix]} #{options[:url]} #{options[:refspec]}"
+        end
+
         message = "Merged lib \"#{options[:libname]}\""
         run "#{gitsubtree} pull --message '#{message}' --prefix #{options[:prefix]} #{options[:url]} #{options[:refspec]}"
     end,
